7e93d2e53053ada5669c22b56c8be065beeb91fb
Alexandre Carneiro <alexcarneiro1993@gmail.com>
2017-09-04 01:43:27 -0300

    changing question_images column to images only

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830033509) do
+ActiveRecord::Schema.define(version: 20170904043531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20170830033509) do
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.json "question_images"
+    t.json "images"
     t.json "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
